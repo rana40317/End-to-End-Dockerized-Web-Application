6b2e257042da32ebeb180ff2886003536f3e5126
rana40317 <sayantankar1@gmail.com>
2026-08-23 18:26:39 +0530

    Version 8 End to End Dockerized Web Application

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from webapp.database import get_connection
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app():
     app = Flask(__name__)
+    PrometheusMetrics(app)
     from webapp.routes import api
     app.register_blueprint(api)
 
