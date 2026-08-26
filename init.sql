c20a52ca1c5d4e9d9ddf8a82bf86554ff9610865
rana40317 <sayantankar1@gmail.com>
2026-08-26 21:35:22 +0530

    Version 9 of End to End Dockerized web Application

--- init.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, status VARCHAR(255)NOT NULL DEFAULT 'pending');
-
-INSERT INTO tasks (title,status) VALUES ('Learn Docker','completed'),('Test production API','pending');
